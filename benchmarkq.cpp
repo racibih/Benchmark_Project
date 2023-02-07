bbcd87c46525f2c485f0fd3cbbb2eecde8f1df77
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-07 18:43:54 -0500

    creating addition function for float benchmark

--- benchmarkq.cpp
+++ benchmarkq.cpp
@@ -7,6 +7,10 @@ using namespace std;
 // 5 × 109 multiplication (of double precision floating point numbers)
 // 2 × 109 division (of double precision floating point numbers)
 int main(){
-
+  auto start = chrono::high_resolution_clock::now();
+    double result = 0.0;
+    for(int i = 0; i < pow(10,10);i++){
+        result +=0.05;
+    }
 
 }

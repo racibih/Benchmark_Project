354fbf0a10aa042e659a144c07b48f6b63ce610a
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-06 20:55:53 -0500

    Adding requirements to file

---
+++ benchmarkp.cpp
@@ -0,0 +1,30 @@
+/* Requirements:
+32-bit Integer operation benchmark (reference time: 100 seconds)
+o 1010 additions (of integer constants)
+o 5 × 109 multiplication (of integer constants)
+o 2 × 109 division (of integer constants)
+ 64-bit Floating point operation benchmark (reference time: 100 seconds)
+o Same as integer, use double precision floating point numbers instead of integer.
+ Memory benchmark (reference time: 100 seconds)
+o Read from 5 × 109 different array elements, 4 bytes each time
+o Write to 5 × 109 different array elements, 4 bytes each time
+ Hard drive benchmark 1 (reference time: 250 seconds)
+o Read a whole file of 109 bytes, 100 bytes each time
+o Write 109 bytes to a file, 100 bytes each time
+ Hard drive benchmark 2 (reference time: 10 seconds)
+o Read a whole file of 109 bytes, 10000 bytes each time
+o Write 109 bytes to a file, 10000 bytes each time
+
+*/
+
+//32 bit integer operation
+//64 bit float operation
+// memory benchmark
+//hard drive benchmark 1
+// hard drive benchmark 2
+int main(){
+
+
+
+    return 0;
+}

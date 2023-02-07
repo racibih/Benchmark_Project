cae2275604d0716ffa5b114f3f027b78ebdeb6c4
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-07 18:37:52 -0500

    creation of 64 bit benchmark file

---
+++ benchmarkq.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+using namespace std;
+//64 bit Floating point operation benchmark
+//o 1010 additions (of double precision floating point numbers)
+// 5 × 109 multiplication (of double precision floating point numbers)
+// 2 × 109 division (of double precision floating point numbers)
+int main(){
+
+
+}

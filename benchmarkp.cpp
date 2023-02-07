11382c8a67c83436f48c17b98e905e8471198921
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-07 17:22:51 -0500

    switching ides test

--- benchmarkp.cpp
+++ benchmarkp.cpp
@@ -14,23 +14,23 @@ o Write 109 bytes to a file, 100 bytes each time
  Hard drive benchmark 2 (reference time: 10 seconds)
 o Read a whole file of 109 bytes, 10000 bytes each time
 o Write 109 bytes to a file, 10000 bytes each time
-
+test
 */
 #include <iostream>
 #include <cmath>
 #include <chrono>
 
 using namespace std;
-chrono::nanoseconds addition_op(int);
-chrono::nanoseconds multiplication_op(int);
-chrono::nanoseconds division_op(int);
+chrono::nanoseconds addition_op(double);
+chrono::nanoseconds multiplication_op(double);
+chrono::nanoseconds division_op(double);
 
 int main(){
 //32 bit integer operation
 //o 1010 additions (of integer constants)
 // 5 × 109 multiplication (of integer constants)
 // 2 × 109 division (of integer constants)
-    int referencetime = 100 * 1000000000; //100 seconds converted to nano
+    double referencetime = (unsigned)100 * 1000000000; //100 seconds converted to nano
 
    
     auto add_duration = addition_op(referencetime);
@@ -44,9 +44,9 @@ int main(){
     return 0;
 }
 
-chrono::nanoseconds addition_op(int referencetime){
+chrono::nanoseconds addition_op(double referencetime){
     auto start = std::chrono::high_resolution_clock::now();
-    int times = pow(10,10);
+    double times = (unsigned) pow(10,10);
     int sum = 0;
      for (int i=0; i < times; i++){
         sum+=i;
@@ -61,9 +61,9 @@ chrono::nanoseconds addition_op(int referencetime){
     auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
   return duration;
 }
-chrono::nanoseconds multiplication_op(int referencetime){
+chrono::nanoseconds multiplication_op(double referencetime){
     auto start = std::chrono::high_resolution_clock::now();
-    int times = 5* pow(10,9);
+    double times = (unsigned) 5* pow(10,9);
     int total = 1;
     
      for (int i=0; i < times; i++){
@@ -79,9 +79,9 @@ chrono::nanoseconds multiplication_op(int referencetime){
     auto duration = chrono::duration_cast<chrono::nanoseconds>(end - start);
   return duration;
 }
-chrono::nanoseconds division_op(int referencetime){
+chrono::nanoseconds division_op(double referencetime){
     auto start = std::chrono::high_resolution_clock::now();
-    int times = 2* pow(10,10);
+    double times = (unsigned) 2* pow(10,10);
     int a = 33;
     int b = 66;
     int result = 0;

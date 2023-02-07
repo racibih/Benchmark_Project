01250a6fc78411c6e0dca55874f79e729b0540a4
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-07 18:55:31 -0500

    setting up fourth benchmark

---
+++ benchmarkr.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+using namespace std;
+/*Memory benchmark (reference time: 100 seconds)
+o Read from 5 × 10^9 different array elements, 4 bytes each time
+o Write to 5 × 10^9 different array elements, 4 bytes each time*/

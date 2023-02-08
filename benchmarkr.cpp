69f6ae73672796357061f6d24c133dd5a664a6eb
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-07 19:26:52 -0500

    reformatting, filling

--- benchmarkr.cpp
+++ benchmarkr.cpp
@@ -5,12 +5,18 @@ using namespace std;
 /*Memory benchmark (reference time: 100 seconds)
 o Read from 5 × 10^9 different array elements, 4 bytes each time
 o Write to 5 × 10^9 different array elements, 4 bytes each time*/
-const int x = (unsigned) 5e9;
+const int x =  5e9;
 const int element_size = 4; // adjust the size to include 4 bytes per element
 
 int main(){
-    char*arr = new char[x*element_size];
-    fill(arr, arr + x*element_size, 0);
+    char *arr = new char[x*element_size];
+    //filling array
+      for (int i = 0; i < x * element_size; i++) {
+    arr[i] = i % 256; //ascii character
+  }
+  //start
+    auto start = chrono::high_resolution_clock::now();
+
 
 return 0;
 }

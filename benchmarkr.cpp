232aef5aced6a99e3ac90b48bb3550a3638b9957
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-07 19:11:27 -0500

    creating main and establishing sizes

--- benchmarkr.cpp
+++ benchmarkr.cpp
@@ -5,3 +5,10 @@ using namespace std;
 /*Memory benchmark (reference time: 100 seconds)
 o Read from 5 × 10^9 different array elements, 4 bytes each time
 o Write to 5 × 10^9 different array elements, 4 bytes each time*/
+int main(){
+const int x = (unsigned) 5e9;
+const int element_size = 4; // adjust the size to include 4 bytes per element
+
+
+return 0;
+}

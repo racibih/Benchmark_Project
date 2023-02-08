939d206efd830324eda5a96b9d3fab5ed6e834fd
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-07 19:41:08 -0500

    writing and deleting dyn array

--- benchmarkr.cpp
+++ benchmarkr.cpp
@@ -13,13 +13,23 @@ int main(){
    
   //start
     auto start = chrono::high_resolution_clock::now();
+    //Reading 4 bytes from an array
     for(int i = 0; i < x;i++){
     char byte1 = arr[i * element_size];
     char byte2 = arr[i * element_size + 1];
     char byte3 = arr[i * element_size + 2];
     char byte4 = arr[i * element_size + 3];
     }
+    //Writing 4 bytes to an array
+     for(int j = 0; j < x;j++){
+    arr[j * element_size] - 0;
+    arr[j * element_size + 1] -0;
+    arr[j * element_size + 2]-0;
+    arr[j * element_size + 3]-0;
+    }
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed_time = end - start;
+    cout<< "Execution time: " << chrono::duration_cast<::chrono::seconds>(elapsed_time).count()<< " seconds"<<endl;
+    delete[] arr;
 return 0;
 }

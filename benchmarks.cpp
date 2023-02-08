2a2a2c6e69112d20d8b5bd20b1d4a4e50c986be1
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-07 20:54:26 -0500

    reading file

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -3,3 +3,17 @@
 #include <chrono>
 
 using namespace std;
+
+const int bfr_sz = 100;
+const int file_sz = 1000000000;  
+
+int main(){
+// Start clock 
+    auto start = chrono::high_resolution_clock::now();
+
+    // Read file
+    char buffer[bfr_sz];
+    ifstream file("testfile.bin", ios::binary);
+
+return 0;
+}

d68d8a39c913a4cae00bd701a5bd6fd045b5e59d
rachel ibihwiori <90010676+racibih@users.noreply.github.com>
2023-02-07 19:14:46 -0500

    filling array elements globalizing variables

--- benchmarkr.cpp
+++ benchmarkr.cpp
@@ -5,10 +5,12 @@ using namespace std;
 /*Memory benchmark (reference time: 100 seconds)
 o Read from 5 × 10^9 different array elements, 4 bytes each time
 o Write to 5 × 10^9 different array elements, 4 bytes each time*/
-int main(){
 const int x = (unsigned) 5e9;
 const int element_size = 4; // adjust the size to include 4 bytes per element
 
+int main(){
+    char*arr = new char[x*element_size];
+    fill(arr, arr + x*element_size, 0);
 
 return 0;
 }
